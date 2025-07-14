1148c3fc12150b95016c006cf2e80d8e04ce982a
Alexandre Barreto <plotartemail@gmail.com>
2025-07-14 17:48:54 -0300

    User profile data retrieved by the token

--- src/app/component/login/login.ts
+++ src/app/component/login/login.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 import { UserService } from '../../service/userService';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { BehaviorSubject, catchError, map, Observable, of, startWith } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  of,
+  startWith,
+} from 'rxjs';
 import { LoginState } from '../../interface/appstates';
 import { DataState } from '../../enum/datastate.enum';
 import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
@@ -12,69 +19,99 @@ import { Key } from '../../enum/key.enum';
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
+  // Observable to hold the login state, initialized with a loaded state, this will be updated based on the login process
+  public loginState$: Observable<LoginState> = of({
+    dataState: DataState.LOADED,
+  });
 
-  public loginState$: Observable<LoginState> = of({ dataState: DataState.LOADED });
-
+  // Subjects to hold phone and email for MFA, used to display the last 4 digits of the phone number and email
+  // when the user is using MFA
   private phoneSubject = new BehaviorSubject<string | null>(null);
   private emailSubject = new BehaviorSubject<string | null>(null);
+
   public readonly DataState = DataState;
 
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService) {}
 
+  /**
+   * This method is called when the user submits the login form. It calls the userService to log in and
+   * handles the response. If the user is using MFA, it updates the phone and email subjects. If the login
+   * is successful, it navigates to the home page.
+   */
   login(loginForm: NgForm): void {
-    this.loginState$ = this.userService.login$(loginForm.value.email, loginForm.value.password)
+    this.loginState$ = this.userService
+      .login$(loginForm.value.email, loginForm.value.password)
       .pipe(
-        map(response => {
+        map((response) => {
           if (response?.data?.user?.usingMfa) {
             this.phoneSubject.next(response.data.user.phone);
             this.emailSubject.next(response.data.user.email);
             return {
-              dataState: DataState.LOADED, isUsingMfa: true, loginSuccess: false,
-              phone: response.data.user.phone.substring(response.data.user.phone.length - 4)
+              dataState: DataState.LOADED,
+              isUsingMfa: true,
+              loginSuccess: false,
+              phone: response.data.user.phone.substring(
+                response.data.user.phone.length - 4
+              ),
             };
           } else {
             localStorage.setItem(Key.TOKEN, response.data.access_token);
-            localStorage.setItem(Key.REFRESH_TOKEN, response.data.refresh_token);
+            localStorage.setItem(
+              Key.REFRESH_TOKEN,
+              response.data.refresh_token
+            );
             this.router.navigate(['/']);
             return { dataState: DataState.LOADED, loginSuccess: true };
           }
         }),
-        startWith(({ dataState: DataState.LOADING, isUsingMfa: false })),
+        startWith({ dataState: DataState.LOADING, isUsingMfa: false }),
         catchError((error: string) => {
-          return of({ dataState: DataState.ERROR, isUsingMfa: false, loginSuccess: false, error })
+          return of({
+            dataState: DataState.ERROR,
+            isUsingMfa: false,
+            loginSuccess: false,
+            error,
+          });
         })
-      )
-
+      );
   }
 
   verifyCode(verifyCodeForm: NgForm): void {
-    this.loginState$ = this.userService.verifyCode$(this.emailSubject.value, verifyCodeForm.value.code)
+    this.loginState$ = this.userService
+      .verifyCode$(this.emailSubject.value, verifyCodeForm.value.code)
       .pipe(
-        map(response => {
+        map((response) => {
           localStorage.setItem(Key.TOKEN, response.data.access_token);
           localStorage.setItem(Key.REFRESH_TOKEN, response.data.refresh_token);
           this.router.navigate(['/']);
           return { dataState: DataState.LOADED, loginSuccess: true };
         }),
         startWith({
-          dataState: DataState.LOADING, isUsingMfa: true, loginSuccess: false,
-          phone: this.phoneSubject.value.substring(this.phoneSubject.value.length - 4)
+          dataState: DataState.LOADING,
+          isUsingMfa: true,
+          loginSuccess: false,
+          phone: this.phoneSubject.value.substring(
+            this.phoneSubject.value.length - 4
+          ),
         }),
         catchError((error: string) => {
           return of({
-            dataState: DataState.ERROR, isUsingMfa: true, loginSuccess: false, error,
-            phone: this.phoneSubject.value.substring(this.phoneSubject.value.length - 4)
-          })
+            dataState: DataState.ERROR,
+            isUsingMfa: true,
+            loginSuccess: false,
+            error,
+            phone: this.phoneSubject.value.substring(
+              this.phoneSubject.value.length - 4
+            ),
+          });
         })
-      )
-
+      );
   }
 
   loginPage(): void {
     this.loginState$ = of({ dataState: DataState.LOADED });
   }
-
 }
--- src/app/service/userService.ts
+++ src/app/service/userService.ts
@@ -1,40 +1,45 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, tap, throwError } from 'rxjs';
 import { CustomHttpResponse, Profiles } from '../interface/appstates';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   private readonly server: string = 'http://localhost:8080';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  login$ = (email: string, password: string) => <Observable<CustomHttpResponse<Profiles>>>
-    this.http.post<CustomHttpResponse<Profiles>>
-      (`${this.server}/user/login`, { email, password })
-      .pipe(
-        tap(console.log),
-        catchError(this.handleError)
-      );
+  login$ = (email: string, password: string) =>
+    <Observable<CustomHttpResponse<Profiles>>>this.http
+      .post<CustomHttpResponse<Profiles>>(`${this.server}/user/login`, {
+        email,
+        password,
+      })
+      .pipe(tap(console.log), catchError(this.handleError));
 
-  verifyCode$ = (email: string, code: string) => <Observable<CustomHttpResponse<Profiles>>>
-    this.http.get<CustomHttpResponse<Profiles>>
-      (`${this.server}/user/verify/code/${email}/${code}`)
-      .pipe(
-        tap(console.log),
-        catchError(this.handleError)
-      );
+  verifyCode$ = (email: string, code: string) =>
+    <Observable<CustomHttpResponse<Profiles>>>(
+      this.http
+        .get<CustomHttpResponse<Profiles>>(
+          `${this.server}/user/verify/code/${email}/${code}`
+        )
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
 
-  profile$ = () => <Observable<CustomHttpResponse<Profiles>>>
-    this.http.get<CustomHttpResponse<Profiles>>
-      (`${this.server}/user/profile`, { headers: new HttpHeaders().set('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJDVVNUT01FUl9NQU5BR0VNRU5UX1NFUlZJQ0UiLCJzdWIiOiJhbGViYXJyZUBob3RtYWlsLmNvbSIsIkFVVEhPUklUSUVTIjpbIlJFQUQ6VVNFUiIsIlJFQUQ6Q1VTVE9NRVIiXSwiaXNzIjoiUEVSRk9STUFOQ0VfU1BPUlRTX0xMQyIsImV4cCI6MTc1MjI4OTQ4NywiaWF0IjoxNzUyMjg3Njg3fQ.-YiMKpfQPAAa05wFLyzbDx-Pm5j669RaoHSUsmK7ZbXzFmmFySYUoYrrbjyXIlVcQe4jhWGOofkFtr6XfikuOQ')})
-      .pipe(
-        tap(console.log),
-        catchError(this.handleError)
-      );
+  profile$ = () => <Observable<CustomHttpResponse<Profiles>>>this.http
+      .get<CustomHttpResponse<Profiles>>(`${this.server}/user/profile`, {
+        headers: new HttpHeaders().set(
+          'Authorization',
+          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJDVVNUT01FUl9NQU5BR0VNRU5UX1NFUlZJQ0UiLCJzdWIiOiJhbGViYXJyZUBob3RtYWlsLmNvbSIsIkFVVEhPUklUSUVTIjpbIlJFQUQ6VVNFUiIsIiBSRUFEOkNVU1RPTUVSIl0sImlzcyI6IlBFUkZPUk1BTkNFX1NQT1JUU19MTEMiLCJleHAiOjE3NTI1Mjc2NzgsImlhdCI6MTc1MjUyNTg3OH0.IHgpEy1Nz1hUD0UhLjsW8HjLwS08RlGAEfZiqYQsvripYEQxw9RdeVnpPyh52dwR4qYiZIEuFGuq-nk-CuQG4g'
+        ),
+      })
+      .pipe(tap(console.log), catchError(this.handleError));
 
   private handleError(error: HttpErrorResponse): Observable<never> {
     let errorMessage: string;
@@ -49,5 +54,4 @@ export class UserService {
     }
     return throwError(() => errorMessage);
   }
-
 }
